dff3318d3625b89dd56e1a3a3cb456eb6fc26014
bestlalala <82032452+bestlalala@users.noreply.github.com>
2021-11-13 04:18:47 +0900

    model success

--- LaundrYours/search/models.py
+++ LaundrYours/search/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Fiber(models.Model):
-    name = models.CharField(max_length=15, default='') #소재
+    name = models.CharField(max_length=20, default='')
     image = models.ImageField(upload_to='images/', default='')
     cleaning = models.TextField(max_length=200, default='') #세탁 방법
-    save = models.TextField(max_length=200, default='', blank=True) #보관 방법
+    saving = models.TextField(max_length=200, default='', blank=True) #보관 방법
     feature = models.TextField(max_length=200, default='', blank=True) #특징
     caution = models.TextField(max_length=200, default='', blank=True) #주의 사항
 
